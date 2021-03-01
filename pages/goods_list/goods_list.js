f0dfd1eb731201dd497cbb33512909a6f43cc618
大维 <oncwnuOyYgnfD2n2Jvko1rcaOyd4@git.weixin.qq.com>
2021-03-01 15:22:41 +0800

    项目基本完成

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -60,7 +60,8 @@ Page({
    */
   onLoad: function (options) {
     //console.log(options);
-    this.QueryParams.cid=options.cid;
+    this.QueryParams.cid=options.cid||"";
+    this.QueryParams.query=options.query||"";
     this.getGoodsList();
 
   },
